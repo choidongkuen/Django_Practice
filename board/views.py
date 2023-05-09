48c7a7c33f9aefafbc31f5f2560d656be6ce3df6
choidongkuen <danaver12@daum.net>
2023-05-09 18:05:41 +0900

    FEAT : index 템플릿 구현 및 관련 view 구현

--- board/views.py
+++ board/views.py
@@ -1,9 +1,15 @@
 # Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from board.models import Question
 
 
 def index(request):
-    return HttpResponse("index 입니다.")
+    latest_question_list = Question.objects.all().order_by('-create_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'board/index.html', context)
+    # return HttpResponse("index 입니다.")
 
 
 def detail(request, board_id):
