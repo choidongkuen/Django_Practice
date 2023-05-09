0bc4e47c15ade5c81fd9b6c7a021a50e756103fd
choidongkuen <danaver12@daum.net>
2023-05-10 00:34:20 +0900

    FEAT : detail view 구현

--- board/views.py
+++ board/views.py
@@ -1,8 +1,11 @@
 # Create your views here.
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from board.models import Question
+
+
+# shortcut -> 단축 함수(내장 함수)
 
 
 def index(request):
@@ -13,7 +16,9 @@ def index(request):
 
 
 def detail(request, board_id):
-    return HttpResponse(str(board_id) + "detail 입니다.")
+    question = get_object_or_404(Question, pk=board_id)
+    return render(request, 'board/detail.html', {"question": question})
+    # return HttpResponse(str(board_id) + "detail 입니다.")
 
 
 def vote(request, board_id):
