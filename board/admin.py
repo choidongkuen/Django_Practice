149b83ba6dae6d14237e2117f00279f98e42b555
choidongkuen <danaver12@daum.net>
2023-05-10 20:23:10 +0900

    FEAT : Admin 페이지 수정

--- board/admin.py
+++ board/admin.py
@@ -2,7 +2,21 @@ from django.contrib import admin
 
 from board.models import Question, Choice
 
-# Register your models here.
 
-admin.site.register(Question)
+# Register your models here.
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['content']}),
+        ('Date Information', {'fields': ['created_at']})
+    ]
+    inlines = [ChoiceInline]  # Questions 테이블 에서 Choice 항목 같이 보기
+    list_display = ('content', 'created_at')
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
--- board/models.py
+++ board/models.py
@@ -5,7 +5,7 @@ from django.db import models
 # 질문 ex) what is your hobby?
 class Question(models.Model):
     content = models.CharField(max_length=200)
-    create_date = models.DateTimeField('date published', auto_now_add=True)
+    created_at = models.DateTimeField('date published')
 
     def __str__(self):
         return self.content
