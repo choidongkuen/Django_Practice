11b369f6e0e341963215bdeb194603441db73bb6
choidongkuen <danaver12@daum.net>
2023-05-11 20:30:58 +0900

    FEAT : [Pybo] index.html 와 detail.html bootstrap 적용

--- pybo/views.py
+++ pybo/views.py
@@ -18,8 +18,4 @@ def detail(request, question_id):
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     question.answer_set.create(content=request.POST.get('content'))
-
-    # answer = Answer(question=question, content=request.POST.get("content"))
-    # answer.save() -> 가능
-
-    return redirect('pybo:detail', question_id=question_id)  # detail redirect
+    return redirect('pybo:detail', question_id=question.id)  # detail redirect
