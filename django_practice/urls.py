8eb5875b5ed406739b22bc07c5e68409f465c915
choidongkuen <danaver12@daum.net>
2023-05-11 15:04:03 +0900

    FEAT : 새로운 app [pybo] 추가 및 urls.py,setting.py 추가

--- django_practice/urls.py
+++ django_practice/urls.py
@@ -25,5 +25,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("board/", include("board.urls"))
+    path("board/", include("board.urls")),
+    path("pybo/", include("pybo.urls"))
 ]
