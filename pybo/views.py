ae5b64647f2fd52d0d48b3c5b1374d510c2d3eff
choidongkuen <danaver12@daum.net>
2023-05-18 19:07:00 +0900

    Revert "Revert "FEAT : [Pybo] Answer(답변) 대한 Comment(댓글) 추가,수정,삭제 관련 view 구현""
    This reverts commit 1eba352adaf05a873dd92f8e1efab9ff3522fed7.

--- pybo/views.py
+++ pybo/views.py
@@ -67,7 +67,7 @@ def question_modify(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
 
     if request.user != question.author:  # 요청한 user 와 해당 question author 다르면
-        messages.error(request, '수정권한이 없습니다.')
+        messages.error(request, '수정 권한이 없습니다.')
         return redirect('pybo:detail', question_id=question.id)
 
     if request.method == "POST":
@@ -88,7 +88,7 @@ def question_modify(request, question_id):
 def question_delete(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     if request.user != question.author:
-        messages.error(request, '삭제권한이 없습니다.')
+        messages.error(request, '삭제 권한이 없습니다.')
         return redirect('pybo:detail', question_id=question.id)
 
     question.delete()
@@ -155,7 +155,7 @@ def comment_update_question(request, comment_id):
     comment = get_object_or_404(Comment, pk=comment_id)
 
     if request.user != comment.author:
-        messages.error(request, '댓글 수정권한이 없습니다.')
+        messages.error(request, '댓글 수정 권한이 없습니다.')
         return redirect('pybo:detail', question_id=comment.question.id)
 
     if request.method == "POST":
@@ -171,6 +171,7 @@ def comment_update_question(request, comment_id):
     return render(request, 'pybo/comment_form.html', context)
 
 
+@login_required(login_url='common:login')
 def comment_delete_question(request, comment_id):
     comment = get_object_or_404(Comment, pk=comment_id)
     if request.user != comment.author:
@@ -179,3 +180,57 @@ def comment_delete_question(request, comment_id):
     else:
         comment.delete()
     return redirect('pybo:detail', question_id=comment.question.id)
+
+
+# 답변 댓글 추가글
+@login_required(login_url="common:login")
+def comment_create_answer(request, answer_id):
+    answer = get_object_or_404(Answer, pk=answer_id)
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.answer = answer
+            comment.save()
+            return redirect('pybo:detail', question_id=comment.answer.question.id)
+    else:
+        form = CommentForm()
+    context = {"form": form}
+    return render(request, 'pybo/comment_form.html', context)  # Comment 작성 html
+
+
+# 답변 댓글 수정
+@login_required(login_url="common:login")
+def comment_update_answer(request, comment_id):
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if request.user != comment.author:
+        messages.error(request, "댓글 수정권한이 없습니다.")
+        return redirect('pybo:detail', question_id=comment.answer.question.id)
+
+    if request.method == "POST":
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.save()
+            return redirect('pybo:detail', question_id=comment.answer.question.id)
+    else:
+        form = CommentForm(instance=comment)
+    context = {'form': form}
+    return render(request, 'pybo/comment_form.html', context)
+
+
+# 답변 댓글 삭제
+@login_required(login_url="common:login")
+def comment_delete_answer(request, comment_id):
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if request.user != comment.author:
+        messages.error(request, '댓글 삭제 권한이 없습니다.')
+        return redirect('pybo:detail', question_id=comment.answer.question.id)
+    else:
+        comment.delete()
+    return redirect('pybo:detail', question_id=comment.answer.question.id)
